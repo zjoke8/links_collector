66858271100b04c940bf322b9e68e361fa0ced1f
zjoke8 <zjoke8@163.com>
2016-02-19 18:33:56 +0800

    delete

--- popup.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-	var data = chrome.extension.getBackgroundPage().links;
-	for i in data{
-		var Html = '<div class="title"></div><div class=""></div>'
-	
-	}
-});
